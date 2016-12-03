2eb55bf363559ec2a4230e9e3c8740b02d6efe73
c007max <c007max@users.noreply.github.com>
2016-12-03 02:50:52 -0500

    Add files via upload

--- src/IOThread_MAIN.java
+++ src/IOThread_MAIN.java
@@ -8,55 +8,59 @@ public class IOThread_MAIN
 	{
 		int nodes_T	= 10 ;
 		PCB	pcbRunning	= null ;
-		PCB pcbMain	= new PCB();
 		Random r1	= new Random();
+		
+		PCB_LL pcb_ll	= new PCB_LL();
 		
 		for (int ii=0; ii<nodes_T; ii++)
 		{
-			pcbRunning = d(new PCB());
-			System.out.printf("\t%d: %s\n"	,ii	,QReady.get(ii).showPCB());
+			PCB pcbMain	= new PCB();
+			pcb_ll.addFirst(pcbMain)	;
+			System.out.printf("\t%d: %s\n"	,ii	,pcbMain.showPCB());
 		}
 
 		CPU_event event = new CPU_event();
-		
-		while (!QReady.isEmpty() || (Thread.activeCount()>1))
-		{
-			pcbRunning	= QReady.removeFirst() ;
-			
-			pcbRunning.add_CPU_used(r1.nextInt(5)+6);
-			
-			if (pcbRunning.get_CPU_left() <= 0)
-			{
-				System.out.printf("***\tmain-0050: (%d) process completed\t used: %d \t max: %d\t***\n"
-						,pcbRunning.get_ID()
-						,pcbRunning.get_CPU_max()
-						,pcbRunning.get_CPU_used()
-						);
-				continue ;				
-			}
-		
-			if (event.get_CPU_event()==2)
-			{
-				Thread iop	= new Thread(new IOProcess
-						(pcbRunning
-						,QReady
-						));
-
-				iop.start();				
-				//	0100
-				System.out.printf("***\tmain-0100: thread started %s %d %s\t***\n"
-						,iop.getName()
-						,iop.getId()
-						,iop.getState()
-						);		
-			}
-			else
-				QReady.addLast(pcbRunning);
-		}
-		
-		for (int ii=0; ii<QReady.size(); ii++)
-			System.out.printf("%s\n"	,QReady.get(ii).showPCB()) ;
-		
+//		
+//		while (!QReady.isEmpty() || (Thread.activeCount()>1))
+//		{
+//			pcbRunning	= QReady.removeFirst() ;
+//			
+//			for
+//			
+//			pcbRunning.add_CPU_used(r1.nextInt(5)+6);
+//			
+//			if (pcbRunning.get_CPU_left() <= 0)
+//			{
+//				System.out.printf("***\tmain-0050: (%d) process completed\t used: %d \t max: %d\t***\n"
+//						,pcbRunning.get_ID()
+//						,pcbRunning.get_CPU_max()
+//						,pcbRunning.get_CPU_used()
+//						);
+//				continue ;				
+//			}
+//		
+//			if (event.get_CPU_event()==2)
+//			{
+//				Thread iop	= new Thread(new IOProcess
+//						(pcbRunning
+//						,QReady
+//						));
+//
+//				iop.start();				
+//				//	0100
+//				System.out.printf("***\tmain-0100: thread started %s %d %s\t***\n"
+//						,iop.getName()
+//						,iop.getId()
+//						,iop.getState()
+//						);		
+//			}
+//			else
+//				QReady.addLast(pcbRunning);
+//		}
+//		
+//		for (int ii=0; ii<QReady.size(); ii++)
+//			System.out.printf("%s\n"	,QReady.get(ii).showPCB()) ;
+//		
 		System.out.printf("@@@\tdone\t@@@\n");
 	}
 }
--- src/PCB_LL.java
+++ src/PCB_LL.java
@@ -14,20 +14,35 @@ public class PCB_LL
 		
 	// get methods
 	
-	public PCB getFirst()
+	public synchronized PCB getFirst()
 	{
 		return QReady.getFirst() ;
 	}
 		
-	public PCB getLast()
+	public synchronized PCB getLast()
 	{
 		return QReady.getLast()	;
 	}
 	
-	public PCB get(int	i0)
+	public synchronized PCB get(int	i0)
 	{
 		return QReady.get(i0)	;
 	}
+	
 	//	set methods
-		}
+	
+	public synchronized void addFirst(PCB pcb0)
+	{
+		QReady.addFirst(pcb0)	;
+	}
+	
+	public synchronized void addLast(PCB pcb0)
+	{
+		QReady.addLast(pcb0)	;
+	}
+	
+	public synchronized void add(int i0	,PCB pcb0)
+	{
+		QReady.add(i0, pcb0)	;
+	}
 }
